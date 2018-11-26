9a3baee8cef434d4ff48ef21f13dd4a8ece74279
Bill ONeill <woneill@pobox.com>
2018-11-26 12:11:28 -0500

    No longer need to specifiy DOCKER_API_VERSION

--- docker/.bash_profile.d/docker.bash
+++ docker/.bash_profile.d/docker.bash
@@ -3,17 +3,12 @@ docker_remote() {
         export DOCKER_HOST="${PRIVATE_DOCKER_HOST}"
     fi
 
-    if [ -n "${PRIVATE_DOCKER_API_VERSION}" ]; then
-        export DOCKER_API_VERSION="${PRIVATE_DOCKER_API_VERSION}"
-    fi
-
     export DOCKER_TLS_VERIFY=1
 }
 
 docker_local() {
     export -n DOCKER_HOST
     export -n DOCKER_TLS_VERIFY
-    export -n DOCKER_API_VERSION
 }
 
 export -f docker_remote docker_local
