9c69c4e0d37b93b93acc9fd4c015bd8a34dd3362
Bill ONeill <bill.oneill@teacherspayteachers.com>
2020-02-24 18:38:00 -0500

    Add script to ensure asdf plugins are installed

---
+++ run_asdf_check.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+command -v asdf >/dev/null 2>&1 && {
+    expected_plugins=(helmfile java kubectl maven nodejs python ruby terraform terraform-docs terraformer tflint tfsec)
+    typeset -A a2
+    for k in $(asdf plugin list); do
+        a2[$k]=1
+    done
+    for k in $expected_plugins; do
+        if [ ! "${a2[$k]}" ]; then
+            asdf plugin add $k
+        fi
+    done
+}
